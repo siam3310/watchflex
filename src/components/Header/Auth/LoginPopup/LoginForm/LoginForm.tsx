0510e582ac78ee18dc7c0ff79fcf9919dc34ad11
Andriy <andremaxico321@gmail.com>
2024-09-25 22:02:08 +0300

    started adding react-hook-form, sending login data

--- src/components/Header/Auth/LoginPopup/LoginForm/LoginForm.tsx
+++ src/components/Header/Auth/LoginPopup/LoginForm/LoginForm.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+'use client'
+
+import React, { useRef } from 'react';
 import styles from './LoginForm.module.scss';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 type PropsType = {};
+type FieldsType = {
+	email: string, 
+	password: string,
+};
 
 export const LoginForm: React.FC<PropsType> = ({}) => {
+	const { register, handleSubmit } = useForm<FieldsType>();
+
+	const formRef = useRef<HTMLFormElement>(null);
+
+	const onSubmit: SubmitHandler<FieldsType> = async data => {
+		console.log(formRef.current);
+		if(formRef.current) {
+			const formData = new FormData(formRef.current);
+			let response = await fetch('/api/auth', {
+				method: 'POST',
+				body: formData,
+			});
+			response = await response.json()
+		}
+	}
+
 	return (
-		<form className={styles.LoginForm}>
+		<form className={styles.LoginForm} onSubmit={handleSubmit(onSubmit)} ref={formRef}>
 			<div className={styles.inputWrap}>
 				<input 
 					placeholder='Електронна пошта'
 					className={styles.input}
 					type='email'
 					required
+					{...register('email')}
 				/>
 			</div>
 			<div className={styles.inputWrap}>
@@ -20,6 +44,7 @@ export const LoginForm: React.FC<PropsType> = ({}) => {
 					className={styles.input}
 					type='password'
 					required
+					{...register('password')}
 				/>
 			</div>
 
