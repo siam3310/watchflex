7ffc8507c91479d6c31a3397dc757de3c795851f
Andriy <andremaxico321@gmail.com>
2025-02-12 22:09:53 +0200

    started adding new review UI

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import styles from './Reviews.module.scss';
 import { axiosInstance } from '@/lib/axios';
 import { Review } from './Review';
 import { RequestReviewsResponseDataModel } from '@/models';
 import cn from 'classnames'
+import { AddPopup } from './AddPopup';
 
 type PropsType = {
     id: string
@@ -18,13 +21,17 @@ const getReviews = async (id: string) => {
 }
 
 export const Reviews: React.FC<PropsType> = async ({id}) => {
+    const [isAddReviewPopupShowing, setIsAddReviewPopupShowing] = useState<boolean>(false);
     const reviews = await getReviews(id) as RequestReviewsResponseDataModel;
 
     console.log(reviews);
 
     return (
         <section className={cn(styles.Reviews, 'container')}>
-            <h3 className={styles.title}>Відгуки</h3>
+            <div className={styles.top}>
+                <h3 className={styles.title}>Відгуки</h3>
+                <button className={styles.addReviewBtn}>+</button>
+            </div>
             <div className={styles.reviewsList}>
                 { reviews.results.length > 0 ?
                     reviews.results.map(data => (
@@ -34,6 +41,7 @@ export const Reviews: React.FC<PropsType> = async ({id}) => {
                     <p className={styles.noText}>Коментарів ще немає</p>
                 }
             </div>
+            <AddPopup isShowing={isAddReviewPopupShowing} />
         </section>
     )
 }
